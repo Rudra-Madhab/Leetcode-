4b5d7e66aaa0ed09e5033f78eb9d7bd9c915021a
RUDRA MADHAB DHAL <rudramadhabdhal@gmail.com>
2026-05-10 23:01:59 +0530

    Added Jump Game 2

---
+++ Gredy.java
@@ -0,0 +1,20 @@
+class Gredy {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int start = 0;
+        int currentTank = 0;
+        int totalTank = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            int diff = gas[i] - cost[i];
+
+            totalTank += diff;
+            currentTank += diff;
+
+            if (currentTank < 0) {
+                start = i + 1;
+                currentTank = 0;
+            }
+        }
+        return (totalTank >= 0) ? start : -1;
+    }
+}
---
+++ JumpGame.java
@@ -0,0 +1,22 @@
+class JumpGame {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        // We don't need to jump from the last index
+        for (int i = 0; i < nums.length - 1; i++) {
+            
+            // Update the farthest reachable index
+            farthest = Math.max(farthest, i + nums[i]);
+
+            // If we reach the end of current jump range
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
